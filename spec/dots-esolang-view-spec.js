5bacf29a2776917915ca27dc4204b544e7783967
Scibuild <zmbx100@gmail.com>
2017-08-12 21:19:05 +1000

    deleting templates

--- spec/dots-esolang-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import DotsEsolangView from '../lib/dots-esolang-view';
-
-describe('DotsEsolangView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
